d3c5cf79c7526697e84f32c08596796cadb17035
Bipulkumar02 <yvipul491@gmail.com>
2026-07-16 12:40:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8080;
 const path = require("path");
 const methodOverride = require("method-override");
 app.use(methodOverride('_method'));
-const{v4:uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
